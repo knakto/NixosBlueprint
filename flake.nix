4759cef07885aff2ee242806b3d60c76ec2a65c4
knakto <kasichonooo@gmail.com>
2025-11-28 16:20:51 +0700

    update: flake.lock | fixed: re format

--- flake.nix
+++ flake.nix
@@ -23,11 +23,9 @@
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = {self, ...} @ inputs: {
+    packages.x86_64-linux.hello = inputs.nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
