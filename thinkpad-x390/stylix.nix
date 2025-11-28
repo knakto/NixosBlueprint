aa850616266f3af67ca43e2edcc801f9bc1c2da6
knakto <kasichonooo@gmail.com>
2025-11-28 17:25:15 +0700

    add: stylix config and import it

---
+++ thinkpad-x390/stylix.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  stylix.image = ../images/1138398.jpg; # <-- ชี้ไปที่ไฟล์รูปในเครื่องนาย
+  # หรือ
+  # stylix.base16Scheme = "path/to/colors.yaml";
+
+  # 2. (บังคับทางอ้อม) บอกขั้วสี (Polarity)
+  stylix.polarity = "dark"; # หรือ "light"
+  stylix.cursor = {
+    package = pkgs.bibata-cursors; # แนะนำตัวนี้! ยอดฮิต เห็นชัด
+    name = "Bibata-Modern-Ice"; # สีขาวขอบดำ (Ice) หรือจะเอาสีดำ (Classic) ก็ได้
+    size = 24; # ขนาดมาตรฐานคือ 24, ถ้าอยากให้ใหญ่สะใจใส่ 32
+  };
+}
